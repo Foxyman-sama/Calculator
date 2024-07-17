27e98354736c05a7010e1f76238d5bbb5fa04eef
Foxyman-sama <dimalatoho1448@gmail.com>
2024-07-17 17:59:11 +0300

    Optimized my tests

--- src/math_input.rs
+++ src/math_input.rs
@@ -22,35 +22,34 @@ impl MathInputImpl {
   }
 
   pub fn calculate(&self, expr: &str) {
-    let mut result = 0;
     let parsed_expr = convert_infix_to_postfix(expr);
-    let mut stack = Vec::new();
+    let mut stack: Vec<String> = Vec::new();
 
     for splitted in parsed_expr.split_whitespace() {
       if is_numeric(splitted) {
-        stack.push(splitted);
+        stack.push(splitted.to_string());
       } else {
         match splitted {
-          "+" => result += sum_two_nums(&mut stack),
-          "-" => result += minus_two_nums(&mut stack),
-          "*" => result += multiply_two_numbers(&mut stack),
-          "/" => result += divide_two_numbers(&mut stack),
+          "+" => sum_two_nums(&mut stack),
+          "-" => minus_two_nums(&mut stack),
+          "*" => multiply_two_numbers(&mut stack),
+          "/" => divide_two_numbers(&mut stack),
           _ => {
-            self.update_output_str_with_error(errors::NON_NUMERIC_ERROR);
+            self.update_output_with_error(errors::NON_NUMERIC_ERROR);
             return;
           }
         }
       }
     }
 
-    self.update_output_num(result)
+    self.update_output(&mut stack)
   }
 
-  fn update_output_num<T: ToString>(&self, new_value: T) {
-    (*self.data_view).borrow_mut().output = new_value.to_string();
+  fn update_output(&self, stack: &mut Vec<String>) {
+    (*self.data_view).borrow_mut().output = stack.pop().unwrap();
   }
 
-  fn update_output_str_with_error(&self, error_msg: &str) {
+  fn update_output_with_error(&self, error_msg: &str) {
     (*self.data_view).borrow_mut().output = String::from(error_msg);
   }
 }
@@ -64,30 +63,34 @@ fn is_numeric(str: &str) -> bool {
   str.parse::<i32>().is_ok()
 }
 
-fn sum_two_nums(stack: &mut Vec<&str>) -> i32 {
+fn sum_two_nums(stack: &mut Vec<String>) {
   let (first, second) = get_two_nums_from_stack(stack);
-  first + second
+  let result = first + second;
+  stack.push(result.to_string())
 }
 
-fn get_two_nums_from_stack(stack: &mut Vec<&str>) -> (i32, i32) {
+fn get_two_nums_from_stack(stack: &mut Vec<String>) -> (i32, i32) {
   let second: i32 = stack.pop().unwrap().parse().unwrap();
   let first: i32 = stack.pop().unwrap().parse().unwrap();
   return (first, second);
 }
 
-fn minus_two_nums(stack: &mut Vec<&str>) -> i32 {
+fn minus_two_nums(stack: &mut Vec<String>) {
   let (first, second) = get_two_nums_from_stack(stack);
-  first - second
+  let result = first - second;
+  stack.push(result.to_string())
 }
 
-fn multiply_two_numbers(stack: &mut Vec<&str>) -> i32 {
+fn multiply_two_numbers(stack: &mut Vec<String>) {
   let (first, second) = get_two_nums_from_stack(stack);
-  first * second
+  let result = first * second;
+  stack.push(result.to_string())
 }
 
-fn divide_two_numbers(stack: &mut Vec<&str>) -> i32 {
+fn divide_two_numbers(stack: &mut Vec<String>) {
   let (first, second) = get_two_nums_from_stack(stack);
-  first / second
+  let result = first / second;
+  stack.push(result.to_string())
 }
 
 #[cfg(test)]
@@ -99,41 +102,6 @@ mod math_input_tests {
     let input = MathInputImpl::new(&data_view);
     (input, data_view)
   }
-  #[test]
-  fn sum_two_numbers() {
-    let (input, data_view) = create_input_and_data_view();
-
-    input.calculate("1 + 2");
-
-    assert_eq!("3", (*data_view).borrow_mut().output);
-  }
-
-  #[test]
-  fn minus_two_numbers() {
-    let (input, data_view) = create_input_and_data_view();
-
-    input.calculate("1 - 2");
-
-    assert_eq!("-1", (*data_view).borrow_mut().output);
-  }
-
-  #[test]
-  fn multiply_two_numbers() {
-    let (input, data_view) = create_input_and_data_view();
-
-    input.calculate("2 * 3");
-
-    assert_eq!("6", (*data_view).borrow_mut().output);
-  }
-
-  #[test]
-  fn divide_two_numbers() {
-    let (input, data_view) = create_input_and_data_view();
-
-    input.calculate("2 / 2");
-
-    assert_eq!("1", (*data_view).borrow_mut().output);
-  }
 
   #[test]
   fn output_contains_error_message_when_expression_contains_non_numeric_chars() {
@@ -142,5 +110,14 @@ mod math_input_tests {
     input.calculate("a + 2");
 
     assert!((*data_view).borrow_mut().output.contains(errors::NON_NUMERIC_ERROR));
+  }
+
+  #[test]
+  fn solve_complex_expression() {
+    let (input, data_view) = create_input_and_data_view();
+
+    input.calculate("25 + (3 * 6) / (2 + 1)");
+
+    assert_eq!("31", (*data_view).borrow_mut().output);
   }
 }
